994fdd54da5d6f9a6d3bfe624f858d0484706cae
Brayan Ceron <brayan0np@gmail.com>
2023-03-26 15:00:53 -0500

    TrbajoFinalUX

---
+++ menu.php
@@ -0,0 +1,73 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Agenda</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php"><i class="bi-house-door"></i> Inicio</a>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="bi-emoji-smile"></i> Usuarios
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="listar_usuarios.php"> Listar </a></li>
+                        <li><a class="dropdown-item" href="add_user1.php">Agregar</a></li>
+                        <li><a class="dropdown-item" href="del_user1.php">Eliminar</a></li>
+                        <li><a class="dropdown-item" href="sesion1.php">Modificar</a></li>
+                        
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+
+
+                        <li><a class="dropdown-item" href="ejercicio1.php">Reporte</a></li>
+                    </ul>
+                </li>
+
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="bi-box2-heart"></i> Formulas
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="tablas1.php">Listar </a></li>
+                        <li><a class="dropdown-item" href="sesion1.php">Agregar</a></li>
+                        <li><a class="dropdown-item" href="sesion1.php">Eliminar</a></li>
+                        <li><a class="dropdown-item" href="sesion1.php">Modificar</a></li>
+                        
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+
+
+                        <li><a class="dropdown-item" href="ejercicio1.php">Reporte</a></li>
+                    </ul>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="bi-yin-yang"></i> Utilidades
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="tablas1.php">Pendiente... </a></li>
+
+                        
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+
+
+                        <li><a class="dropdown-item" href="ejercicio1.php">Reporte</a></li>
+                    </ul>
+                </li>
+
+            </ul>
+
+        </div>
+    </div>
+</nav>
